8aba6ed85e2d98a56da6399a2535ac126c6385dc
柒玖 <2394172829@qq.com>
2020-08-18 19:02:59 +0800

    ok

---
+++ usr/themes/Mirages/usr/License.php
@@ -0,0 +1,14 @@
+<?php
+if (!defined('__TYPECHO_ROOT_DIR__')) exit;
+
+/**
+ * License.php
+ * Author     : Hran
+ * Date       : 2020/07/19
+ * Version    :
+ * Description:
+ */
+class License {
+    static $name = "Mirages";
+    static $pk = "ODlBNkkxOjE1OTUxNDkxMzA6MzEyMThmOGNkZGZiY2QzNTQ0OGNkODI2YmM5MjIyNDY1";
+}
